b78eb38b9e13082137fd384cd908e933a3917c3a
Ankitagupta454 <ayaush122@gmail.com>
2021-05-13 22:56:50 +0530

    typing speed test

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+             <a
+                href="https://github.com/AnkitaGupta454"
+                className="footer-link"
+                target="_blank"
+                rel="noreferrer"
+            >
+
+                Visit my Github Profile Here
+            </a>
+        </div>
+    );
+};
+
+export default Footer;
